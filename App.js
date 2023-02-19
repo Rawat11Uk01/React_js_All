c8d020ab906ec9a0beea94d5855674fbe64ac89c
alind <kashishrawat308@gmail.com>
2023-02-19 17:54:29 +0530

    babel remove console.logs

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-const heading1 = React.createElement('h1',{},'i am h1')
-    const heading2 = React.createElement('h2',{},'i am h2')
+import React from "react";
+import ReactDOM from "react-dom/client ";
+const heading1 = React.createElement('h1',{key:'1'},'i am parcel')
+    const heading2 = React.createElement('h2',{key:'2'},'i am h2')
     const div = React.createElement('div',{},[heading1,heading2])
     const root =  ReactDOM.createRoot(document.getElementById("root"));
     root.render(div)
