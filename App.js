a66d15fcf7f0be3ad46ba20077ee5fb9d56e849f
alind <kashishrawat308@gmail.com>
2023-02-19 22:18:28 +0530

    rendering component

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client ";
-const heading1 = React.createElement('h1',{key:'1'},'i am parcel')
-    const heading2 = React.createElement('h2',{key:'2'},'i am h2')
-    const div = React.createElement('div',{},[heading1,heading2])
-    const root =  ReactDOM.createRoot(document.getElementById("root"));
-    root.render(div)
+const heading3 = <h1>ok good</h1>;
+const FunctionalComponent = ()=>{
+  return <><h1>What's up</h1></>
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<FunctionalComponent/>);
 
 
-      // const heading = React.createElement("h1", {class:'ok'}, "i am learning yo");
-      // console.log(heading,'headingheading')
-      // const root = ReactDOM.createRoot(document.getElementById("root"));
-      // root.render(heading);
+// const heading = React.createElement("h1", {class:'ok'}, "i am learning yo");
+// console.log(heading,'headingheading')
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
